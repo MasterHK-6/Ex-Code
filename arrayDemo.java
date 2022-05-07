47060f5967cab51544442b18b1d726d3ffdfa7b0
MasterHK-6 <105093695+MasterHK-6@users.noreply.github.com>
2022-05-07 09:17:00 +0530

    Add files via upload

---
+++ arrayDemo.java
@@ -0,0 +1,20 @@
+class arrayDemo{
+	public static void main(String[] args) {
+		int a[]=new int[5],temp;
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+
+		System.out.println("Array is: ");
+		for(int i=0;i<5;i++){
+			System.out.println(a[i]);
+		}
+
+		System.out.println("Array is: ");
+		for(int i=0;i<5;i++){
+
+		}
+	}
+}
